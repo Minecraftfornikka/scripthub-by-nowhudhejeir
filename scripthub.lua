7289830cae70d4f83f8c1831413f9d18f8a92bfa
Minecraftfornikka <abobatopaha@gmail.com>
2025-02-06 18:03:05 +0400

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -436,25 +436,7 @@ local Button = Tab:CreateButton({
 local Button = Tab:CreateButton({
     Name = "hitbox (head)",
     Callback = function()
-   _G.HeadSize = 50
-_G.Disabled = true
- 
-game:GetService('RunService').RenderStepped:connect(function()
-if _G.Disabled then
-for i,v in next, game:GetService('Players'):GetPlayers() do
-if v.Name ~= game:GetService('Players').LocalPlayer.Name then
-pcall(function()
-v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
-v.Character.HumanoidRootPart.Transparency = 0.7
-v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
-v.Character.HumanoidRootPart.Material = "Neon"
-v.Character.HumanoidRootPart.CanCollide = false
-end)
-end
-end
-end
-end) 		
-  	end
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/Minecraftfornikka/Hitbox.lua/refs/heads/main/Hitbox.lua'))()
     end,    
 })
 
